455f15219f9a9e92816a4ab3197ace726657ab2b
WuHaibo <whb@h77.(none)>
2013-01-27 17:08:08 +0800

    Add twitter-bootstrap gems

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cost::Application.config.secret_token = 'c333d2f3ce41bbfca8a407a9ec5df736212e5633827f166e1499f58ebd4f52d30ce00713bb30578455f408c3fc6772ed56b47425015da7467b0bb60a013c2c22'
