b139a9af2d52d84f71a19df6341a4b40e837395b
WuHaibo <wuhaibo@xllg.com>
2013-02-23 10:46:14 +0800

    Beauty expense test data

--- lib/tasks/expense_data.rb
+++ lib/tasks/expense_data.rb
@@ -3,6 +3,8 @@
 desc "Add expense bills"
 task :expenses => :environment do
   create_list :expense, 50
+  create_list :expense, 100, status: :reimbursed, explain: "已报销xxxxxxxxxxxxxx"
+  create_list :expense, 10, status: :invalid, explain: "作废xxxxxxxxxxx"
 end
 
 desc "Run expense tasks"
--- lib/tasks/factories.rb
+++ lib/tasks/factories.rb
@@ -34,11 +34,12 @@ FactoryGirl.define do
 
   factory :expense do
     sn { Expense.generate_sn }
+    organization_id { [2, 4, 5, 6].sample }
     sequence(:abstract) { |n| "摘要及......用途#{n}" }
     request_on { (1.month.ago.to_date..Date.today).to_a.sample }
-    sequence(:staff) { |n| "用户#{n}" }
-    organization_id { [2, 4, 5, 6].sample }
-    status { [:edit, :commit].sample }
+    staff { ["张三", "李四", "王五", "赵六"].sample }
+    sequence(:explain) { |n| "填写支出理由、用途说明、购买要求等: 设备已经报废，需要更新; 该经费用于购买演出服装; 请注意机箱的钢板厚度;设备已经报废，需要更新; 该经费用于购买演出服装; 请注意机箱的钢板厚度;《#{n}》" }
+    status { [:edit, :commit, :manager_approval, :general_manager_approval].sample }
 
     after(:build) do |expense|
       expense.items << FactoryGirl.build_list(:item, rand(1..5), expense: expense)
