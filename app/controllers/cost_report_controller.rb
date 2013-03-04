3c04a738711868ba38ae17e29c1b1590dcb5b25f
WuHaibo <wuhaibo@xllg.com>
2013-03-04 17:00:54 +0800

    basic finish cost_report

--- app/controllers/cost_report_controller.rb
+++ app/controllers/cost_report_controller.rb
@@ -8,12 +8,7 @@ class CostReportController < ApplicationController
       @organization_amount_hash[o] = []
 
       1.upto(12).each do | month |
-        begin_date = Date.new(Date.today.year , month, 1)
-        if (month == 12)
-          end_date = Date.new(Date.today.year+1 , 1, 1)
-        else
-          end_date = Date.new(Date.today.year , month+1, 1)
-        end
+        begin_date, end_date = month_begin_end_date month 
         sum_amount = Reimbursement.where({:reimburse_on => begin_date...end_date, :organization_id => o.id}).sum(:amount)
 
         @organization_amount_hash[o][month] = sum_amount
@@ -28,18 +23,24 @@ class CostReportController < ApplicationController
       @category_amount_hash[c] = []
 
       1.upto(12).each do | month |
-        begin_date = Date.new(Date.today.year , month, 1)
-        if (month == 12)
-          end_date = Date.new(Date.today.year+1 , 1, 1)
-        else
-          end_date = Date.new(Date.today.year , month+1, 1)
-        end
-
-        sum_amount = Reimbursement.where({:reimburse_on => begin_date...end_date}).sum(:amount)
+        begin_date, end_date = month_begin_end_date month 
+        sum_amount = Detail.joins(:reimbursement).where({'reimbursements.reimburse_on' => begin_date...end_date, :category_id => c.id})
+                           .sum(:price)
 
         @category_amount_hash[c][month] = sum_amount
       end
     end
-    
+  end
+
+  private
+  def month_begin_end_date(month)
+    this_year = Date.today.year
+    begin_date = Date.new(this_year, month, 1)
+    if (month == 12)
+      end_date = Date.new(this_year + 1, 1, 1)
+    else
+      end_date = Date.new(this_year, month+1, 1)
+    end
+    return begin_date, end_date
   end
 end
