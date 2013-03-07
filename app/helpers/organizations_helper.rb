d8929bc1e95472810ebe6e282d1d11a760584b82
WuHaibo <wuhaibo@xllg.com>
2013-03-07 17:09:49 +0800

    try kind

--- app/helpers/organizations_helper.rb
+++ app/helpers/organizations_helper.rb
@@ -6,4 +6,9 @@ module OrganizationsHelper
   def kind_types
     Organization::KIND_TYPES.map { |k, v| [ v, k ] }
   end
+
+  def display_kind(organization)
+    return ' ' if organization.kind
+    t "organizations.KIND_TYPES.#{organization.kind}" 
+  end
 end
