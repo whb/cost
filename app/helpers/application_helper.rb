eb6b4a16ddc1b0ed1f7799fbc59f4e53f8683d9d
WuHaibo <wuhaibo@xllg.com>
2013-03-09 10:55:05 +0800

    Refactor application helper to cost report helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -56,55 +56,8 @@ module ApplicationHelper
     @current_controller == name
   end
 
-  def smart_detail_list_path(category, organization, month)
-    category_id = category ? category.id : '*'
-    organization_id = organization ? organization.id : '*'
-    detail_list_path(category_id, organization_id, month)
-  end
-
-  def link_details_query(amount, category, organization, month)
-    return '' unless amount
-    link_to humanized_money(amount), smart_detail_list_path(category, organization, '*')
-  end
-
-  def smart_reimbursement_list(category, organization, month)
-    category_id = category ? category.id : '*'
-    organization_id = organization ? organization.id : '*'
-    reimbursement_list_path(category_id, organization_id, month)
-  end
-
-  def link_reimbursement_list(amount, category, organization, month)
-    return '' unless amount
-    link_to humanized_money(amount), smart_reimbursement_list(category, organization, month)
-  end
-
-  def css_of_detail(detail, selected_category) 
+  def css_of_detail(detail, selected_category)
     detail.category == selected_category ? 'info' : ''
-  end
-
-  def detail_list_title(category, organization, month)
-    category_name = category ? category.name : t('All Categories')
-    organization_name = organization ? organization.name : t('All Organizations')
-    month_name = (month == '*') ? t('Whole Year') : month.to_s + t('month')
-    organization_name + '(' + category_name + ')' + ' : ' + month_name + t('Cost Details')
-  end
-
-  def organization_cost_title(organization)
-    organization_name = organization ? organization.name : t('All Organizations')
-    organization_name + t('.title')
-  end
-
-
-  def reimbursement_list_title(category, organization, month)
-    category_name = category ? category.name : t('All Categories')
-    organization_name = organization ? organization.name : t('All Organizations')
-    month_name = (month == '*') ? t('Whole Year') : month.to_s + t('month')
-    organization_name + '(' + category_name + ')' + ' : ' + month_name + t('Reimbursement List')
-  end
-
-  def category_cost_title(category)
-    category_name = category ? category.name : t('All Categories')
-    category_name + t('.title')
   end
 
   def humanized_money(money)
--- app/helpers/cost_report_helper.rb
+++ app/helpers/cost_report_helper.rb
@@ -1,2 +1,48 @@
 module CostReportHelper
+  def smart_detail_list_path(category, organization, month)
+    category_id = category ? category.id : '*'
+    organization_id = organization ? organization.id : '*'
+    detail_list_path(category_id, organization_id, month)
+  end
+
+  def link_details_query(amount, category, organization, month)
+    return '' unless amount
+    link_to humanized_money(amount), smart_detail_list_path(category, organization, '*')
+  end
+
+  def smart_reimbursement_list(category, organization, month)
+    category_id = category ? category.id : '*'
+    organization_id = organization ? organization.id : '*'
+    reimbursement_list_path(category_id, organization_id, month)
+  end
+
+  def link_reimbursement_list(amount, category, organization, month)
+    return '' unless amount
+    link_to humanized_money(amount), smart_reimbursement_list(category, organization, month)
+  end
+
+
+  def detail_list_title(category, organization, month)
+    category_name = category ? category.name : t('All Categories')
+    organization_name = organization ? organization.name : t('All Organizations')
+    month_name = (month == '*') ? t('Whole Year') : month.to_s + t('month')
+    organization_name + '(' + category_name + ')' + ' : ' + month_name + t('Cost Details')
+  end
+
+  def reimbursement_list_title(category, organization, month)
+    category_name = category ? category.name : t('All Categories')
+    organization_name = organization ? organization.name : t('All Organizations')
+    month_name = (month == '*') ? t('Whole Year') : month.to_s + t('month')
+    organization_name + '(' + category_name + ')' + ' : ' + month_name + t('Reimbursement List')
+  end
+
+  def organization_cost_title(organization)
+    organization_name = organization ? organization.name : t('All Organizations')
+    organization_name + t('.title')
+  end
+
+  def category_cost_title(category)
+    category_name = category ? category.name : t('All Categories')
+    category_name + t('.title')
+  end
 end
