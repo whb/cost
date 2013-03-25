5a9d20236387d2b77c4d1a922ca59799a6ff89cc
WuHaibo <wuhaibo@xllg.com>
2013-03-25 21:00:37 +0800

    auto populate leaf categoreis bill data

--- lib/tasks/factories.rb
+++ lib/tasks/factories.rb
@@ -26,7 +26,7 @@ FactoryGirl.define do
 
   trait :item_detail_part do
 
-    category_id  { ($three_level_categories.map {|c| c.id}).sample } 
+    category_id  { ($leaf_categories.map {|c| c.id}).sample } 
     sequence(:name) { |n| "费用名称......#{n}" }
     amount { rand(1..5) }
     unit { ["个", "箱", "台", "套"].sample }
