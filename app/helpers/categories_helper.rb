6b265234de6a49b7823e33486e8dd4599a97d434
WuHaibo <wuhaibo@xllg.com>
2013-03-25 15:05:24 +0800

    Beautify category

--- app/helpers/categories_helper.rb
+++ app/helpers/categories_helper.rb
@@ -2,4 +2,8 @@ module CategoriesHelper
   def major_category_name(category)
     category.major_category ? category.major_category.name : ""
   end
+
+  def superior_category_name(category)
+    category.superior == nil ? "" : category.superior.code_name
+  end
 end
