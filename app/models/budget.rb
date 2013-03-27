bfd598708c29af6a17b9ffd416fc859e7bafa90f
WuHaibo <wuhaibo@xllg.com>
2013-03-27 10:06:52 +0800

    Optimize performance match_this_year(category)

--- app/models/budget.rb
+++ app/models/budget.rb
@@ -7,11 +7,13 @@ class Budget < ActiveRecord::Base
   default_scope :include => :category, :order => "categories.code"
   scope :this_year, joins(:period).where('periods.year' => Date.today.year)
 
-  after_save :clear_cache_of_match_budget_category_hash
-  after_destroy :clear_cache_of_match_budget_category_hash
+  after_save :clear_cache
+  after_destroy :clear_cache
 
-  def clear_cache_of_match_budget_category_hash
+  def clear_cache
     Category.clear_cache_of_match_budget_category_hash
+    @@match_this_year = nil
+    @@is_cached = nil
   end
 
   def available
@@ -23,6 +25,14 @@ class Budget < ActiveRecord::Base
   end
 
   def self.match_this_year(category)
+    @@match_this_year ||= {}
+    @@is_cached ||= {}
+    return  @@match_this_year[category.id] if @@is_cached[category.id]
+    @@is_cached[category.id] = true
+    @@match_this_year[category.id] = find_match_this_year(category)
+  end
+
+  def self.find_match_this_year(category)
     b = self.this_year.find_by_category_id(category)
     return b if (b && b.amount)
     self.match_this_year(category.superior) if category.superior
